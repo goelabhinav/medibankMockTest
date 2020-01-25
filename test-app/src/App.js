5651224a610f0ade65c70bbe706bda08e6bf3122
Abhinav Goel <abhigoel08@gmail.com>
2020-01-26 10:20:18 +1100

    partially working code

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -1,26 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    catRegistration:[
+      {
+        "name": "Bob",
+        "gender": "Male",
+        "age": 23,
+        "pets": [{"name": "Garfield", "type": "Cat"}, {"name": "Fido", "type": "Dog"}]
+      },
+      {"name": "Jennifer", "gender": "Female", "age": 18, "pets": [{"name": "Garfield", "type": "Cat"}]},
+      {"name": "Steve", "gender": "Male", "age": 45, "pets": null},
+      {
+        "name": "Fred",
+        "gender": "Male",
+        "age": 40,
+        "pets": [
+          {"name": "Tom", "type": "Cat"},
+          {"name": "Max", "type": "Cat"},
+          {"name": "Sam", "type": "Dog"},
+          {"name": "Jim", "type": "Cat"}
+        ]
+      },
+      {"name": "Samantha", "gender": "Female", "age": 40, "pets": [{"name": "Tabby", "type": "Cat"}]},
+      {
+        "name": "Alice",
+        "gender": "Female",
+        "age": 64,
+        "pets": [{"name": "Simba", "type": "Cat"}, {"name": "Nemo", "type": "Fish"}]
+      }
+    ]
+  };
+
+  catNamerFilterMale(){
+    return this.state.catRegistration.filter((e)=> e.gender === "Male" && e.pets != null).map(ee=>{
+      return(
+        <div>
+          {/* <p>The Owner's name is : {ee.name}</p> */}
+          {/* <p>The Owner's gender is : {ee.gender}</p> */}
+          <p>{ee.pets.filter(e=>e.type=="Cat").map(e=>e.name).join("\n")}</p>
+        </div>
+      )
+    })
+  }
+
+  catNamerFilterFemale(){
+    return this.state.catRegistration.filter((e)=> e.gender === "Female" && e.pets != null).map(ee=>{
+      return(
+        <div>
+          {/* <p>The Owner's name is : {ee.name}</p>
+          <p>The Owner's gender is : {ee.gender}</p> */}
+          <p>{ee.pets.filter(e=>e.type=="Cat").map(e=>e.name).sort(e=>e.name)}</p>
+          {console.log(ee.pets.filter(e=>e.type=="Cat").map(e=>e.name).sort(e=>e.name))}
+        </div>
+      )
+    })
+  }
+
+
+  render(){
+    return (
+      <div>
+        <p>Male</p>
+        {this.catNamerFilterMale()}
+        <p>Female</p>
+        {this.catNamerFilterFemale()}
+      </div>
+    )
+  }
 }
 
 export default App;
