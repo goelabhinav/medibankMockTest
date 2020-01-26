2a0bcc944a6197b74745ae08ac8da3989971dc31
Abhinav Goel <abhigoel08@gmail.com>
2020-01-27 00:29:41 +1100

    Fixed linked fetching

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -2,73 +2,55 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-
-  state = {
-    catRegistration:[
-      {
-        "name": "Bob",
-        "gender": "Male",
-        "age": 23,
-        "pets": [{"name": "Garfield", "type": "Cat"}, {"name": "Fido", "type": "Dog"}]
-      },
-      {"name": "Jennifer", "gender": "Female", "age": 18, "pets": [{"name": "Garfield", "type": "Cat"}]},
-      {"name": "Steve", "gender": "Male", "age": 45, "pets": null},
-      {
-        "name": "Fred",
-        "gender": "Male",
-        "age": 40,
-        "pets": [
-          {"name": "Tom", "type": "Cat"},
-          {"name": "Max", "type": "Cat"},
-          {"name": "Sam", "type": "Dog"},
-          {"name": "Jim", "type": "Cat"}
-        ]
-      },
-      {"name": "Samantha", "gender": "Female", "age": 40, "pets": [{"name": "Tabby", "type": "Cat"}]},
-      {
-        "name": "Alice",
-        "gender": "Female",
-        "age": 64,
-        "pets": [{"name": "Simba", "type": "Cat"}, {"name": "Nemo", "type": "Fish"}]
-      }
-    ]
-  };
-
-  catNamerFilterMale(){
-    return this.state.catRegistration.filter((e)=> e.gender === "Male" && e.pets != null).map(ee=>{
-      return(
-        <div>
-          {/* <p>The Owner's name is : {ee.name}</p> */}
-          {/* <p>The Owner's gender is : {ee.gender}</p> */}
-          <p>{ee.pets.filter(e=>e.type=="Cat").map(e=>e.name).join("\n")}</p>
-        </div>
-      )
-    })
+  constructor(props){
+    super(props);
+    this.state = {
+      items: [],
+      isLoaded : false,
+    }
   }
 
-  catNamerFilterFemale(){
-    return this.state.catRegistration.filter((e)=> e.gender === "Female" && e.pets != null).map(ee=>{
-      return(
-        <div>
-          {/* <p>The Owner's name is : {ee.name}</p>
-          <p>The Owner's gender is : {ee.gender}</p> */}
-          <p>{ee.pets.filter(e=>e.type=="Cat").map(e=>e.name).sort(e=>e.name)}</p>
-          {console.log(ee.pets.filter(e=>e.type=="Cat").map(e=>e.name).sort(e=>e.name))}
-        </div>
-      )
-    })
+  componentDidMount(){
+    const url="https://gist.githubusercontent.com/medibank-digital/a1fc81a93200a7b9d5f8b7eae0fac6f8/raw/de10a4fcf717e6c431e88c965072c784808fd6b2/people.json";
+    fetch(url)
+    .then(res=> res.json())
+    .then(json => { 
+      this.setState({
+        items    :   json,
+        isLoaded :   true,
+      })
+    });
+    
   }
 
 
-  render(){
-    return (
+   render(){
+    var {isLoaded, items} = this.state;
+    if(!isLoaded){
+      return <div>Loading...</div>
+    }
+    else{
+      return (
       <div>
-        <p>Male</p>
-        {this.catNamerFilterMale()}
-        <p>Female</p>
-        {this.catNamerFilterFemale()}
-      </div>
-    )
+          <b>MALE</b>
+          <ul>
+          {items.filter((e)=> e.gender === "Male" && e.pets != null).
+          map((item)=>(item.pets.filter(e=>e.type ==="Cat").
+          map(e=>(e.name))).sort().join("  ")).sort().join(" \n")}
+          </ul>
+
+
+
+             <b>Male</b> 
+         {items.filter((e)=> e.gender === "Male" && e.pets != null).map((ee, index) => {
+          return <p>{ee.pets.filter(e=>e.type ==="Cat").map((e)=> {
+            return <li>{e.name}</li>
+          })}</p>
+         })}
+      </div>   
+      )
+    }
+
   }
 }
 
